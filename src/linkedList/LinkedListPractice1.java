c7677b431bb512f9d30c03b506e68c2f8573163e
mylhb9 <mylhb9@naver.com>
2022-02-21 23:27:42 +0900

    자료구조 LinkedList

---
+++ src/linkedList/LinkedListPractice1.java
@@ -0,0 +1,91 @@
+package linkedList;
+
+public class LinkedListPractice1<T> {
+
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if (this.head == null) {
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public void printAll() {
+        if (this.head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public T searchFromHead(T isData) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public T searchFromTail(T isData) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.tail;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.prev;
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListPractice1<Integer> MyLinkedList = new LinkedListPractice1<Integer>();
+
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+
+        MyLinkedList.printAll();
+
+        System.out.println(MyLinkedList.searchFromHead(3));
+        System.out.println(MyLinkedList.searchFromTail(1));
+        System.out.println(MyLinkedList.searchFromTail(6));
+    }
+}
