8dd17398b89a7accb4964b27e8e2d41bfc7fe87a
mylhb9 <mylhb9@naver.com>
2022-02-27 01:48:34 +0900

    알고리즘 이진 탐색 연습

---
+++ src/binarySearch/BinarySearch.java
@@ -0,0 +1,38 @@
+package binarySearch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BinarySearch {
+    public boolean searchFunc(ArrayList<Integer> dataList, Integer searchItem) {
+        if(dataList.size() == 1 && searchItem == dataList.get(0)) {
+            return true;
+        }
+
+        if(dataList.size() == 1 && searchItem != dataList.get(0)) {
+            return false;
+        }
+
+        if(dataList.size() == 0) {
+            return false;
+        }
+
+        int medium = dataList.size() / 2;
+        if(dataList.get(medium) > searchItem) {
+            return this.searchFunc(new ArrayList<>(dataList.subList(0, medium)), searchItem);
+        } else {
+            return this.searchFunc(new ArrayList<>(dataList.subList(medium, dataList.size())), searchItem);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<>();
+        for(int i=0; i<100; i++) {
+            testData.add((int)(Math.random() * 100));
+        }
+
+        Collections.sort(testData);
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.searchFunc(testData, 2));
+    }
+}
