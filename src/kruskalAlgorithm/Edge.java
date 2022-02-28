892b3f77094188c0604b0eace7b4efe4c3abf733
mylhb9 <mylhb9@naver.com>
2022-02-28 17:00:54 +0900

    알고리즘 kruskalAlgorithm

---
+++ src/kruskalAlgorithm/Edge.java
@@ -0,0 +1,55 @@
+package kruskalAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Edge implements Comparable<Edge>{
+    public int weight;
+    public String nodeU;
+    public String nodeV;
+
+    public Edge(int weight, String nodeU, String nodeV) {
+        this.weight = weight;
+        this.nodeU = nodeU;
+        this.nodeV = nodeV;
+    }
+
+    public String toString() {
+        return "(" + this.weight + ", " + this.nodeV + ", " + this.nodeU + ")";
+    }
+
+    @Override
+    public int compareTo(Edge edge) {
+        return this.weight - edge.weight;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> vetices = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G"));
+        ArrayList<Edge> edges = new ArrayList<>();
+        edges.add(new Edge(7, "A", "B"));
+        edges.add(new Edge(5, "A", "D"));
+        edges.add(new Edge(7, "B", "A"));
+        edges.add(new Edge(8, "B", "C"));
+        edges.add(new Edge(9, "B", "D"));
+        edges.add(new Edge(7, "B", "E"));
+        edges.add(new Edge(8, "C", "B"));
+        edges.add(new Edge(5, "C", "E"));
+        edges.add(new Edge(5, "D", "A"));
+        edges.add(new Edge(9, "D", "B"));
+        edges.add(new Edge(7, "D", "E"));
+        edges.add(new Edge(6, "D", "F"));
+        edges.add(new Edge(7, "E", "B"));
+        edges.add(new Edge(5, "E", "C"));
+        edges.add(new Edge(7, "E", "D"));
+        edges.add(new Edge(8, "E", "F"));
+        edges.add(new Edge(9, "E", "G"));
+        edges.add(new Edge(6, "F", "D"));
+        edges.add(new Edge(8, "F", "E"));
+        edges.add(new Edge(11, "F", "G"));
+        edges.add(new Edge(9, "G", "E"));
+        edges.add(new Edge(11, "G", "F"));
+
+        System.out.println(vetices);
+        System.out.println(edges);
+    }
+}
