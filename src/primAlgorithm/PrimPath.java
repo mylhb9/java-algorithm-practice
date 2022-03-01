d3441f5a0e93992cec418ec426e41aec4a33bcbd
mylhb9 <mylhb9@naver.com>
2022-03-01 23:07:05 +0900

    알고리즘 primAlgorithm 연습1

--- src/primAlgorithm/PrimPath.java
+++ src/primAlgorithm/PrimPath.java
@@ -78,4 +78,22 @@ public class PrimPath {
         }
         return mst;
     }
+
+    public static void main(String[] args) {
+        ArrayList<Edge> myedges = new ArrayList<Edge>();
+        myedges.add(new Edge(7, "A", "B"));
+        myedges.add(new Edge(5, "A", "D"));
+        myedges.add(new Edge(8, "B", "C"));
+        myedges.add(new Edge(9, "B", "D"));
+        myedges.add(new Edge(7, "D", "E"));
+        myedges.add(new Edge(5, "C", "E"));
+        myedges.add(new Edge(7, "B", "E"));
+        myedges.add(new Edge(6, "D", "F"));
+        myedges.add(new Edge(8, "E", "F"));
+        myedges.add(new Edge(9, "E", "G"));
+        myedges.add(new Edge(11, "F", "G"));
+
+        PrimPath pObject = new PrimPath();
+        System.out.println(pObject.primFunc("A", myedges));
+    }
 }
