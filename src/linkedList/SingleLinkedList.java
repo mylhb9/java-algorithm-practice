fe668efde9f9edd49c04a7fff9f25c77cb57bf30
mylhb9 <mylhb9@naver.com>
2022-03-03 13:50:08 +0900

    자료구조 LinkedList 복습

---
+++ src/linkedList/SingleLinkedList.java
@@ -0,0 +1,73 @@
+package linkedList;
+
+public class SingleLinkedList<T> {
+    public Node<T> head = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if(head == null) {
+            head = new Node<>(data);
+        } else {
+            Node<T> node = this.head;
+            while(node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<>(data);
+        }
+    }
+
+    public void printAll() {
+        if(head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while(node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> myLinkedList = new SingleLinkedList<>();
+        myLinkedList.addNode(1);
+        myLinkedList.addNode(2);
+        myLinkedList.addNode(3);
+
+        myLinkedList.printAll();
+    }
+
+    public Node<T> search(T data) {
+        if(this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node.next != null) {
+                if(node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.search(isData);
+        if(searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+}
