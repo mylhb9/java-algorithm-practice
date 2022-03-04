76de28c996f908fed8feb2257065dd01cbf8db8e
mylhb9 <mylhb9@naver.com>
2022-03-05 08:40:05 +0900

    알고리즘 버블정렬 복습

---
+++ src/sortingAlgorithm/BubbleSort.java
@@ -0,0 +1,33 @@
+package sortingAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BubbleSort {
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+        for(int i=0; i<dataList.size()-1; i++) {
+            boolean swap = false;
+
+            for(int j=0; j<dataList.size()-1-i; j++) {
+                if(dataList.get(j) > dataList.get(j+1)) {
+                    Collections.swap(dataList, j, j+1);
+                    swap = true;
+                }
+            }
+
+            if(!swap) {
+                break;
+            }
+        }
+        return dataList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            testData.add((int)(Math.random() * 100));
+        }
+        BubbleSort bSort = new BubbleSort();
+        System.out.println(bSort.sort(testData));
+    }
+}
