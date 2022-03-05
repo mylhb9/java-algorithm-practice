72b302eba079d34e9c7f7b64e0f8fb1cbbdcc82a
mylhb9 <mylhb9@naver.com>
2022-03-05 20:30:50 +0900

    알고리즘 퀵정렬 복습

--- src/quickSort/Split.java
+++ src/quickSort/Split.java
@@ -7,30 +7,32 @@ public class Split {
     public void splitFunc(ArrayList<Integer> dataList) {
         if(dataList.size() <= 1) {
             return;
-        }
+        } else {
+            int pivot = dataList.get(0);
+            ArrayList<Integer> leftArr = new ArrayList<>();
+            ArrayList<Integer> rightArr = new ArrayList<>();
 
-        int pivot = dataList.get(0);
-        ArrayList<Integer> leftArr = new ArrayList<>();
-        ArrayList<Integer> rightArr = new ArrayList<>();
-
-        for(int i=1; i<dataList.size(); i++) {
-            if(pivot > dataList.get(i)) {
-                leftArr.add(dataList.get(i));
-            } else {
-                rightArr.add(dataList.get(i));
+            // pivot 과 겹치지 않기 위해서 1부터 넣어줘야한다.
+            for(int i=1; i<dataList.size(); i++) {
+                if(dataList.get(i) > pivot) {
+                    rightArr.add(dataList.get(i));
+                } else {
+                    leftArr.add(dataList.get(i));
+                }
             }
+
+            ArrayList<Integer> mergedArr = new ArrayList<>();
+            mergedArr.addAll(leftArr);
+            mergedArr.addAll(Arrays.asList(pivot));
+            mergedArr.addAll(rightArr);
+
+            System.out.println(leftArr);
+            System.out.println(pivot);
+            System.out.println(rightArr);
+            System.out.println(mergedArr);
         }
 
-        ArrayList<Integer> mergedArr = new ArrayList<>();
-        mergedArr.addAll(leftArr);
-        mergedArr.addAll(Arrays.asList(pivot));
-        mergedArr.addAll(rightArr);
 
-
-        System.out.println(leftArr);
-        System.out.println(pivot);
-        System.out.println(rightArr);
-        System.out.println(mergedArr);
     }
 
     public static void main(String[] args) {
