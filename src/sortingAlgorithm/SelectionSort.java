c7582a37a24ba9076a460f096457ed655fb99b02
mylhb9 <mylhb9@naver.com>
2022-03-05 09:02:10 +0900

    알고리즘 선택정렬 복습

---
+++ src/sortingAlgorithm/SelectionSort.java
@@ -0,0 +1,30 @@
+package sortingAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SelectionSort {
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+        int lowest = 0;
+        for(int i=0; i<dataList.size(); i++) {
+            lowest = i;
+            for(int j=i; j<dataList.size(); j++) {
+                if(dataList.get(j) < dataList.get(lowest)) {
+                    lowest = j;
+                }
+            }
+            Collections.swap(dataList, i, lowest);
+        }
+        return dataList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            testData.add((int)(Math.random() * 100));
+        }
+        SelectionSort sSort = new SelectionSort();
+        System.out.println(sSort.sort(testData));
+    }
+}
