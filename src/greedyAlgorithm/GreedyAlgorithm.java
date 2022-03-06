306b108c8cf498cb29e99617305ab22b44db8831
mylhb9 <mylhb9@naver.com>
2022-03-06 10:55:19 +0900

    알고리즘 탐욕 알고리즘 knapsack 복습

--- src/greedyAlgorithm/GreedyAlgorithm.java
+++ src/greedyAlgorithm/GreedyAlgorithm.java
@@ -3,7 +3,7 @@ package greedyAlgorithm;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class FractionalKnapsackProblem {
+public class GreedyAlgorithm {
     public void knapsackFunc(Integer[][] objectList, double capacity) {
         double totalValue = 0.0;
         double fraction = 0.0;
@@ -11,18 +11,18 @@ public class FractionalKnapsackProblem {
         Arrays.sort(objectList, new Comparator<Integer[]>() {
             @Override
             public int compare(Integer[] o1, Integer[] o2) {
-                return (o2[1] / o2[0]) - (o1[1] / o1[0]);
+                return o2[1]/o2[0] - o1[1]/o1[0];
             }
         });
 
         for(int i=0; i<objectList.length; i++) {
-            if(capacity - (double) objectList[i][0] > 0) {
-                capacity -= (double) objectList[i][0];
-                totalValue += (double) objectList[i][1];
+            if(capacity - objectList[i][0] > 0) {
+                capacity -= objectList[i][0];
+                totalValue += objectList[i][1];
                 System.out.println("무게: " + objectList[i][0] + ", 가치: " + objectList[i][1]);
             } else {
-                fraction = capacity / (double) objectList[i][0];
-                totalValue += objectList[i][1] * fraction;
+                fraction = capacity / objectList[i][0];
+                totalValue += fraction * objectList[i][1];
                 System.out.println("무게: " + objectList[i][0] + ", 가치: " + objectList[i][1] + ", 비율: " + fraction);
                 break;
             }
@@ -31,7 +31,7 @@ public class FractionalKnapsackProblem {
     }
 
     public static void main(String[] args) {
-        FractionalKnapsackProblem gObject = new FractionalKnapsackProblem();
+        GreedyAlgorithm gObject = new GreedyAlgorithm();
         Integer[][] objectList = {{10, 10}, {15, 12}, {20, 10}, {25,8}, {30, 5}};
         gObject.knapsackFunc(objectList, 30.0);
     }
