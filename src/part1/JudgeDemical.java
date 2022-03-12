33acf9fbd3c437421624e99bc1f1d924905a2eaa
mylhb9 <mylhb9@naver.com>
2022-03-13 02:33:37 +0900

    알고리즘 소수판별

---
+++ src/part1/JudgeDemical.java
@@ -0,0 +1,19 @@
+package part1;
+
+import java.util.Scanner;
+
+public class JudgeDemical {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=2; i<n; i++) {
+            if(n % i == 0) {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
