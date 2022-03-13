15434d22478d4708ae256c65d33492bd035d0abd
mylhb9 <mylhb9@naver.com>
2022-03-13 14:03:45 +0900

    알고리즘 배수의 개수 세기

---
+++ src/part1/EvenCnt.java
@@ -0,0 +1,30 @@
+package part1;
+
+import java.util.Scanner;
+
+public class EvenCnt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] strArr = sc.nextLine().split(" ");
+        int A = Integer.parseInt(strArr[0]);
+        int B = Integer.parseInt(strArr[1]);
+        int C = Integer.parseInt(strArr[2]);
+
+        int count = 0;
+        if(B > A) {
+            for(int i=A; i<=B; i++) {
+                if(i % C == 0) {
+                    count++;
+                }
+            }
+        } else {
+            for(int i=B; i<=A; i++) {
+                if(i % C == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
