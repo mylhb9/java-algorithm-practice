4e33ad2ba5e8d1bb9bb9d2fa15899bf006ab338e
mylhb9 <mylhb9@naver.com>
2022-03-13 16:38:12 +0900

    알고리즘 삼각형 출력3

---
+++ src/part2/Triangle3.java
@@ -0,0 +1,20 @@
+package part2;
+
+import java.util.Scanner;
+
+public class Triangle3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n-1-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
