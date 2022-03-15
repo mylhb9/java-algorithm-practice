36e0ad86a15adac478ed3e8903d0fe652ef1f0ec
mylhb9 <mylhb9@naver.com>
2022-03-15 22:08:12 +0900

    알고리즘 level1 짝수 판별하기

---
+++ src/level1/JudgeEven.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.Scanner;
+
+public class JudgeEven {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        if(N % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("not even");
+        }
+    }
+}
