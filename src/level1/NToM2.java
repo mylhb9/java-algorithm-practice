a03362eda014d272288493698f76f6dae41f6fac
mylhb9 <mylhb9@naver.com>
2022-03-15 22:14:38 +0900

    알고리즘 level1 NToM2

---
+++ src/level1/NToM2.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.Scanner;
+
+public class NToM2 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] strArr = sc.nextLine().split(" ");
+        int N = Integer.parseInt(strArr[0]);
+        int M = Integer.parseInt(strArr[1]);
+
+        for(int i=N; i<=M; i++) {
+            if((i-N+1) % 8 == 0) {
+                System.out.println(i);
+            } else {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
