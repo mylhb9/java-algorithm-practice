ac749403b9271ea6f97e19212e426119b5c14063
mylhb9 <mylhb9@naver.com>
2022-03-17 13:07:46 +0900

    알고리즘 part6 순열구하기

--- src/part6/SolvePermutationEx.java
+++ src/part6/SolvePermutationEx.java
@@ -1,23 +1,25 @@
 package part6;
 
+import java.util.Arrays;
 import java.util.Scanner;
-public class SolvePermutation {
 
-    int n;
-    int r;
-    char[] result = new char[10];
-    boolean[] checkArr = {false, false, false, false, false, false, false, false, false, false};
+public class SolvePermutationEx {
+    int N;
+    int R;
+    char[] result = new char[50];
+    boolean[] checkArr = new boolean[50];
+
 
     public void getResult(int x) {
-        if(x >= r) {
+        if(x >= R) {
             for(int i=0; i<result.length; i++) {
                 if(result[i] != 0) {
-                    System.out.print(result[i]);
+                    System.out.print(result[i] + " ");
                 }
             }
             System.out.println();
         } else {
-            for(int i=0; i<n; i++) {
+            for(int i=0; i<N; i++) {
                 char alpha = (char) (i + 'a');
                 if(!checkArr[i]) {
                     result[x] = alpha;
@@ -30,17 +32,17 @@ public class SolvePermutation {
         }
     }
 
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String [] strArr = sc.nextLine().split(" ");
 
-        int n = Integer.parseInt(strArr[0]);
-        int r = Integer.parseInt(strArr[1]);
-        SolvePermutation m = new SolvePermutation();
-        m.n = n;
-        m.r = r;
-        m.getResult(0);
+        String[] strArr = sc.nextLine().split(" ");
+        int N = Integer.parseInt(strArr[0]);
+        int R = Integer.parseInt(strArr[1]);
+        SolvePermutationEx sp = new SolvePermutationEx();
+        sp.N = N;
+        sp.R = R;
+        Arrays.fill(sp.checkArr, false);
+        sp.getResult(0);
+
     }
 }
