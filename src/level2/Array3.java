4067bc7e778d262f0a2ca57dfe837f8334ee351e
mylhb9 <mylhb9@naver.com>
2022-03-18 01:40:51 +0900

    알고리즘 level2 Array3

--- src/level2/Array3.java
+++ src/level2/Array3.java
@@ -1,4 +1,28 @@
 package level2;
 
+import java.util.Scanner;
+
 public class Array3 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+
+        int sum = 1;
+        int beta = 1;
+        for(int i=0; i<N; i++) {
+            int pivot = sum;
+            int alpha = i;
+            for(int j=0; j<N-i; j++) {
+                sum += alpha;
+                alpha++;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+            sum = pivot;
+            sum += beta;
+            beta++;
+        }
+    }
 }
