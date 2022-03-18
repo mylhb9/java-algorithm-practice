16dcf310d9104738e81dc7cdf04314ab715f763a
mylhb9 <mylhb9@naver.com>
2022-03-18 17:05:28 +0900

    알고리즘 level3 mine

---
+++ src/level3/Mine.java
@@ -0,0 +1,36 @@
+package level3;
+
+
+import java.util.Scanner;
+
+public class Mine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] mapSize = sc.nextLine().split(" ");
+        int N = Integer.parseInt(mapSize[0]);
+        int M = Integer.parseInt(mapSize[1]);
+
+        String[] clickPosition = sc.nextLine().split(" ");
+        int x = Integer.parseInt(clickPosition[0])-1;
+        int y = Integer.parseInt(clickPosition[1])-1;
+
+        String[][] strMap = new String[N][M];
+        Integer[][] map = new Integer[N][M];
+
+        for(int i=0; i<N; i++) {
+            strMap[i] = sc.nextLine().split(" ");
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                map[i][j] = Integer.parseInt(strMap[i][j]);
+            }
+        }
+        if(map[x][y] == 0) {
+            int sum = map[x-1][y-1] + map[x-1][y] + map[x-1][y+1] + map[x][y-1] + map[x][y+1] + map[x+1][y-1] + map[x+1][y] + map[x+1][y+1];
+            System.out.println(sum);
+        } else {
+            System.out.println("game over");
+        }
+    }
+}
