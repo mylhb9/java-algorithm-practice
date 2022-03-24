c0c8f2db190a57166734f214b987db9238a3f6e1
mylhb9 <mylhb9@naver.com>
2022-03-25 06:11:05 +0900

    알고리즘 part7 괄호의값 다시풀이

---
+++ src/part7/BracketValue1.java
@@ -0,0 +1,88 @@
+package part7;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BracketValue1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Stack<Integer> stack = new Stack<>();
+        char[] inputArr = sc.nextLine().toCharArray();
+
+        for(int i=0; i<inputArr.length; i++) {
+            if(inputArr[i] == '(' || inputArr[i] == '[') {
+                if(inputArr[i] == '(') {
+                    stack.push(-1);
+                } else {
+                    stack.push(-2);
+                }
+            } else {
+                if(stack.size() == 0) {
+                    System.out.println(0);
+                    return;
+                }
+
+                if(inputArr[i] == ')') {
+                    if(stack.peek() == -1) {
+                        stack.pop();
+                        stack.push(2);
+                    } else if(stack.peek() == -2) {
+                        System.out.println(0);
+                        return;
+                    } else {
+                        int pivot = stack.pop();
+                        if(stack.size() != 0) {
+                            if(stack.pop() == -1) {
+                                stack.push(pivot * 2);
+                            } else if(stack.peek() == -2) {
+                                System.out.println(0);
+                                return;
+                            }
+                        }
+
+                    }
+                    if(stack.size() >= 2 && stack.peek() != -1 && stack.peek() != -2) {
+                        int pivot = stack.pop();
+                        if(stack.peek() != -2 && stack.peek() != -1) {
+                            stack.push(stack.pop() + pivot);
+                        } else {
+                            stack.push(pivot);
+                        }
+                    }
+                } else {
+                    if(stack.peek() == -1) {
+                        System.out.println(0);
+                        return;
+                    } else if(stack.peek() == -2) {
+                        stack.pop();
+                        stack.push(3);
+                    } else {
+                        int pivot = stack.pop();
+                        if(stack.size() != 0) {
+                            if(stack.pop() == -2) {
+                                stack.push(pivot * 3);
+                            } else if(stack.peek() == -1) {
+                                System.out.println(0);
+                                return;
+                            }
+                        }
+                    }
+                    if(stack.size() >= 2 && stack.peek() != -1 && stack.peek() != -2) {
+                        int pivot = stack.pop();
+                        if(stack.peek() != -2 && stack.peek() != -1) {
+                            stack.push(stack.pop() + pivot);
+                        } else {
+                            stack.push(pivot);
+                        }
+                    }
+                }
+            }
+        }
+        if(stack.size() == 1 && stack.get(0) > 0) {
+            System.out.println(stack.get(0));
+        } else {
+            System.out.println(0);
+        }
+    }
+}
