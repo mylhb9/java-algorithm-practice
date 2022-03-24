17734c40ad72db60e768657ad1f4d37f92b58742
mylhb9 <mylhb9@naver.com>
2022-03-24 16:32:22 +0900

    알고리즘 part7 괄호의 값 수정

--- src/part7/BracketValue.java
+++ src/part7/BracketValue.java
@@ -10,39 +10,44 @@ public class BracketValue {
         String[] strArr = sc.nextLine().split("");
 
         Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<strArr.length; i++) {
-            if(strArr[i].equals("(") || strArr[i].equals("[")) {
-                if(strArr[i].equals("(")) {
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].equals("(") || strArr[i].equals("[")) {
+                if (strArr[i].equals("(")) {
                     stack.push(-1);
                 } else {
                     stack.push(-2);
                 }
             } else {
-                if(stack.empty()) {
+                if (stack.size() == 0) {
                     System.out.println(0);
                     return;
                 }
-                if(strArr[i].equals(")")) {
-                    if(stack.peek() == -1) {
+                if (strArr[i].equals(")")) {
+                    if (stack.peek() == -1) {
                         stack.pop();
                         stack.push(2);
-                    } else if(stack.peek() == -2) {
+                    } else if (stack.peek() == -2) {
                         System.out.println(0);
                         return;
                     } else {
                         int pivot = stack.pop();
-                        if(stack.peek() == -1) {
-                            stack.pop();
-                            stack.push( pivot * 2);
-                        } else if(stack.peek() == -2) {
+                        if (stack.size() != 0) {
+                            if (stack.peek() == -1) {
+                                stack.pop();
+                                stack.push(pivot * 2);
+                            } else if (stack.peek() == -2) {
+                                System.out.println(0);
+                                return;
+                            }
+                        } else {
                             System.out.println(0);
                             return;
                         }
                     }
-                    if(stack.size() >= 2) {
-                        if(stack.peek() != -1 && stack.peek() != -2) {
+                    if (stack.size() >= 2) {
+                        if (stack.peek() != -1 && stack.peek() != -2) {
                             int pivot = stack.pop();
-                            if(stack.peek() != -1 && stack.peek() != -2) {
+                            if (stack.peek() != -1 && stack.peek() != -2) {
                                 stack.push(stack.pop() + pivot);
                             } else {
                                 stack.push(pivot);
@@ -50,26 +55,31 @@ public class BracketValue {
                         }
                     }
                 } else {
-                    if(stack.peek() == -2) {
+                    if (stack.peek() == -2) {
                         stack.pop();
                         stack.push(3);
-                    } else if(stack.peek() == -1) {
+                    } else if (stack.peek() == -1) {
                         System.out.println(0);
                         return;
                     } else {
                         int pivot = stack.pop();
-                        if(stack.peek() == -2) {
-                            stack.pop();
-                            stack.push( pivot * 3);
-                        } else if(stack.peek() == -1) {
+                        if (stack.size() != 0) {
+                            if (stack.peek() == -2) {
+                                stack.pop();
+                                stack.push(pivot * 3);
+                            } else if (stack.peek() == -1) {
+                                System.out.println(0);
+                                return;
+                            }
+                        } else {
                             System.out.println(0);
                             return;
                         }
                     }
-                    if(stack.size() >= 2) {
-                        if(stack.peek() != -1 && stack.peek() != -2) {
+                    if (stack.size() >= 2) {
+                        if (stack.peek() != -1 && stack.peek() != -2) {
                             int pivot = stack.pop();
-                            if(stack.peek() != -1 && stack.peek() != -2) {
+                            if (stack.peek() != -1 && stack.peek() != -2) {
                                 stack.push(stack.pop() + pivot);
                             } else {
                                 stack.push(pivot);
@@ -80,7 +90,7 @@ public class BracketValue {
             }
 
         }
-        if(stack.size() == 1 && stack.get(0) > 0) {
+        if (stack.size() == 1 && stack.get(0) > 0) {
             System.out.println(stack.pop());
         } else {
             System.out.println(0);
