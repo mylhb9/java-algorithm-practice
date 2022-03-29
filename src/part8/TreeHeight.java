b1a2229408ff6fc2155dd5e448bf082872bec0b3
mylhb9 <mylhb9@naver.com>
2022-03-29 23:19:09 +0900

    알고리즘 part8 트리의 높이

---
+++ src/part8/TreeHeight.java
@@ -0,0 +1,61 @@
+package part8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Tree2 {
+    int parent;
+}
+
+public class TreeHeight {
+    List<Integer> countList = new ArrayList<>();
+    int r;
+    int count = 1;
+    Tree2[] tree = new Tree2[100];
+
+
+    void findHeight(int x) {
+        if(tree[x].parent == r) {
+            countList.add(count);
+            count = 1;
+        } else {
+            count++;
+            findHeight(tree[x].parent);
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] strArr = sc.nextLine().split(" ");
+        int n = Integer.parseInt(strArr[0]);
+        int r = Integer.parseInt(strArr[1]);
+
+        Tree2[] tree = new Tree2[100];
+        for(int i=0; i<100; i++) {
+            tree[i] = new Tree2();
+        }
+        String[][] inputArr = new String[n-1][2];
+        for(int i=0; i<n-1; i++) {
+            inputArr[i] = sc.nextLine().split(" ");
+            tree[Integer.parseInt(inputArr[i][1])].parent = Integer.parseInt(inputArr[i][0]);
+        }
+        TreeHeight m = new TreeHeight();
+        m.tree = tree;
+        m.r = r;
+
+        for(int i=0; i<n-1; i++) {
+            m.findHeight(Integer.parseInt(inputArr[i][1]));
+        }
+        System.out.println(m.countList);
+        int height = 0;
+        for(int i=0; i<m.countList.size(); i++) {
+            height = Math.max(height, m.countList.get(i));
+        }
+        System.out.println(height);
+
+    }
+}
