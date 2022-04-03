230c6cc981341c4644b990cef39eccb2dfe9448c
mylhb9 <mylhb9@naver.com>
2022-04-04 03:07:48 +0900

    알고리즘 level5 combinationzero

--- src/level5/CombinationZero.java
+++ src/level5/CombinationZero.java
@@ -2,47 +2,25 @@ package level5;
 
 import java.util.Scanner;
 
+// 문제: nCm 의 끝자리 0의 개수를 출력하는 프로그램을 만들어라
+// n! 은 소인수 몇개로 구성되어 있는지 알 수 있다.
+// 따라서 10의 소인수들의 개수로 n! 에 0의 개수가 몇개인지 추론이 가능하고 그에 따른 계산이 가능하다
 public class CombinationZero {
-    Integer[] notes;
-    public int factorial(int x) {
-        if(x == 1) {
-            return 1;
-        } else {
-            if(notes[x] != null) {
-                return notes[x];
-            }
-            notes[x] = x * factorial(x - 1);
-            return notes[x];
+    public static int getNumOfDigits(int n, int x) {
+        int answer = 0;
+        int temp = x;
+        while(temp <= n) {
+            answer += n / temp;
+            temp *= x;
         }
+        return answer;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String[] strArr = sc.nextLine().split(" ");
         int n = Integer.parseInt(strArr[0]);
         int m = Integer.parseInt(strArr[1]);
 
-        CombinationZero c = new CombinationZero();
-        c.notes = new Integer[1000010];
-        long parent = 1;
-        for(int i=n; i>n-m; i--) {
-            parent *= i;
-        }
-        int son = c.factorial(m);
-        System.out.println(son);
-        long answer  = parent / son;
-
-        int count = 0;
-        while(true) {
-            if(answer % 10 == 0) {
-                answer /= 10;
-                count++;
-            } else {
-                break;
-            }
-
-        }
-        System.out.println(count);
+        System.out.println(Math.min(getNumOfDigits(n, 2) - getNumOfDigits(m, 2) - getNumOfDigits(n-m, 2), getNumOfDigits(n, 5) - getNumOfDigits(m, 5) - getNumOfDigits(n-m, 5)));
     }
 }
