c450e150820ccef73cce51c0634ed36f88017478
mylhb9 <mylhb9@naver.com>
2022-04-04 17:28:47 +0900

    알고리즘 level6 큰자릿수곱셈

--- src/level6/LargeDigitsMultiplication1.java
+++ src/level6/LargeDigitsMultiplication1.java
@@ -9,7 +9,7 @@ package level6;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class LargeDigitsMultiplication {
+public class LargeDigitsMultiplication1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
---
+++ src/level6/LargeDigitsMultiplication2.java
@@ -0,0 +1,15 @@
+package level6;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LargeDigitsMultiplication2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(sc.nextLine());
+        BigInteger b = new BigInteger(sc.nextLine());
+
+        System.out.println(a.multiply(b));
+    }
+}
