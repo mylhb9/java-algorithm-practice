f8cb857b64fc26f3b213cca5769ac19d2338a7f8
mylhb9 <mylhb9@naver.com>
2022-04-09 17:09:09 +0900

    알고리즘 study division

--- src/part6/Main.java
+++ src/part6/Main.java
@@ -1,7 +1,53 @@
 package part6;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    int n;
+    boolean[] visited;
+    Integer[] arr;
+    int count = 0;
+    void recur(int x, int sum, int start) {
+        if(sum > n || x>n) {
+            return;
+        }
+
+        if(sum == n) {
+            for(int i=0; i<n; i++) {
+                if(arr[i] != null) {
+                    if(arr[i+1] == null) {
+                        System.out.print(arr[i]);
+                    } else {
+                        System.out.print(arr[i]+"+");
+                    }
+                }
+            }
+            count++;
+            System.out.println();
+        } else {
+            for(int i=start; i>=1; i--) {
+                arr[x] = i;
+                sum += i;
+                recur(x+1, sum, start);
+                sum -= i;
+                arr[x] = null;
+                start-=1;
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Main m = new Main();
+        m.n = n;
+        m.visited = new boolean[21];
+        Arrays.fill(m.visited, false);
+        m.arr = new Integer[21];
+        m.recur(0, 0, n-1);
+        System.out.println(m.count);
     }
 }
