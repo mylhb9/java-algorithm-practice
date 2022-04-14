deca452de94e1c8ddde35406bba3122d436e8d99
mylhb9 <mylhb9@naver.com>
2022-04-14 18:02:53 +0900

    알고리즘 Level 9 합병정렬 구현하기

--- src/level9/ImplementMergeSorting.java
+++ src/level9/ImplementMergeSorting.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 // 문제: n개의 정수가 주어질 떄, 이를 합병정렬을 이용하여 오름차순으로 정렬하는 프로그램을 작성
 // 입력: 첫 번째 줄에 n이 주어진다. (1 <= n <= 100,000) 두번째 줄에 n개의 정수가 주어진다.
 // 출력: 첫 번째 줄에 n개의 숫자를 합병정렬을 이용하여 오름차순으로 정렬한 결과를 출력
-public class Main {
+public class ImplementMergeSorting {
     Integer[] temp = new Integer[100001];
 
     public void merging(Integer[] arr, int start1, int end1, int start2, int end2) {
@@ -69,7 +69,7 @@ public class Main {
         }
 
 
-        Main m = new Main();
+        ImplementMergeSorting m = new ImplementMergeSorting();
         m.mergeSort(inputArr, 0, n-1);
         for(int i=0; i<n; i++) {
             System.out.print(inputArr[i] + " ");
