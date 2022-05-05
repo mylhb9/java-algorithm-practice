41f7d207fde6b575e9e7b727e44aafb4ceb7a093
mylhb9 <mylhb9@naver.com>
2022-05-05 17:45:58 +0900

    알고리즘 part9 직사각형 배치의 경우의 수 +

--- src/part9/NumberOfCasesOfRectangularArrangement4.java
+++ src/part9/NumberOfCasesOfRectangularArrangement4.java
@@ -3,24 +3,16 @@ package part9;
 import java.util.Scanner;
 
 public class NumberOfCasesOfRectangularArrangement4 {
-    int[] notes = new int[101];
-    public int getFactorial(int N) {
-        if(N==1) {
-            return 1;
-        } else if(N==2) {
-            return 2;
-        } else {
-            if(notes[N] == 0) {
-                notes[N] = (getFactorial(N-1) % 1000007 + getFactorial(N-2) % 1000007) % 1000007;
-            }
-            return notes[N];
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        NumberOfCasesOfRectangularArrangement4 m = new NumberOfCasesOfRectangularArrangement4();
-        System.out.println(m.getFactorial(N));
+        int[] notes = new int[101];
+        notes[1] = 1;
+        notes[2] = 2;
+        for(int i=3; i<=N; i++) {
+            notes[i] = (notes[i-1] + notes[i-2]) % 1000007;
+        }
+        System.out.println(notes[N]);
     }
 }
