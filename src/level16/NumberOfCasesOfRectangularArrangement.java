47ca0b76810e3b0418a13c46fcc69e2850ed3307
mylhb9 <mylhb9@naver.com>
2022-05-10 02:54:54 +0900

    알고리즘 level16 직사각형배치의경우의수

---
+++ src/level16/NumberOfCasesOfRectangularArrangement.java
@@ -0,0 +1,21 @@
+package level16;
+
+import java.util.Scanner;
+
+public class NumberOfCasesOfRectangularArrangement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N =sc.nextInt();
+
+        int[] dp = new int[101];
+        dp[1] = 1;
+        dp[2] = 2;
+        if(N>=3) {
+            for(int i=3; i<=N; i++) {
+                dp[i] = (dp[i-1] + dp[i-2]) % 1000007;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
