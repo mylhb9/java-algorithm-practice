a1c5c12ca27c2c6269b0a4d2f87221a581951fd6
mylhb9 <mylhb9@naver.com>
2022-05-17 12:11:49 +0900

    알고리즘 level20 파티

--- src/level20/Party.java
+++ src/level20/Party.java
@@ -2,82 +2,82 @@ package level20;
 
 import java.util.*;
 
-class Edge {
-    int start;
-    int end;
-    int value;
-}
-
-class MyComparator implements Comparator<Edge> {
-    @Override
-    public int compare(Edge e1, Edge e2) {
-        if(e1.value > e2.value) {
-            return 1;
-        }
-        return -1;
-    }
-}
-
 public class Party {
-    int[] parent = new int[1001];
-    public static Edge makeEdges(int a, int b, int c) {
-        Edge edge = new Edge();
-        edge.start = a;
-        edge.end = b;
-        edge.value = c;
-        return edge;
-    }
-
-    public int find(int x) {
-        if(x == parent[x]) {
-            return x;
-        } else {
-            int y = find(parent[x]);
-            parent[x] = y;
-            return y;
+    Map<Integer, List<Integer>> graph;
+    Map<Integer, List<Integer>> costs;
+    int N;
+    public int[] dijkstra(int start) {
+        boolean[] check = new boolean[1001];
+        int[] table = new int[1001];
+        for(int i=1; i<=N; i++) {
+            table[i] = 987987987;
         }
-    }
 
-    public boolean union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
+        table[start] = 0;
 
-        if(rootA == rootB) {
-            return false;
-        } else {
-            parent[rootA] = rootB;
+        for(int i=0; i<N; i++) {
+            int min = 987987987;
+            int minIdx = -1;
 
-            return true;
+            for(int j=1; j<=N; j++) {
+                if(!check[j] && table[j] < min) {
+                    min = table[j];
+                    minIdx = j;
+                }
+            }
+
+            check[minIdx] = true;
+
+            for(int j=0; j<graph.get(minIdx).size(); j++) {
+                int node = graph.get(minIdx).get(j);
+                int weight = costs.get(minIdx).get(j);
+                if(table[node] > table[minIdx] + weight) {
+                    table[node] = table[minIdx] + weight;
+                }
+            }
         }
+        return table;
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        // 정점의 개수
         int N = sc.nextInt();
+        // 간선의 개수
         int M = sc.nextInt();
+        // 철수의 집 번호
         int K = sc.nextInt();
-        List<Edge> edgeList = new ArrayList<>();
-        for(int i=0; i<M; i++) {
-            edgeList.add(makeEdges(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, List<Integer>> costs = new HashMap<>();
+        Map<Integer, List<Integer>> revGraph = new HashMap<>();
+
+        for(int i=1; i<=N; i++) {
+            graph.put(i, new ArrayList<>());
+            costs.put(i, new ArrayList<>());
+            revGraph.put(i, new ArrayList<>());
         }
-        MyComparator myComparator = new MyComparator();
-        Collections.sort(edgeList, myComparator);
+
+        for(int i=0; i<M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(a).add(b);
+            costs.get(a).add(c);
+            revGraph.get(b).add(a);
+        }
 
         Party m = new Party();
-        for(int i=1; i<=N; i++) {
-            m.parent[i] = i;
-        }
+        m.graph = graph;
+        m.costs = costs;
+        m.N = N;
 
-        int result = 0;
 
-        for(int i=0; i<M; i++) {
-            if(m.union(K, edgeList.get(i).end)) {
-                result += edgeList.get(i).value;
-            }
-        }
+        int[] table1 = new int[1001];
+        int[] table2 = new int[1001];
+        table1 = m.dijkstra(K);
+        m.graph = revGraph;
+        table2 = m.dijkstra(K);
 
-        System.out.println(result);
     }
 }
