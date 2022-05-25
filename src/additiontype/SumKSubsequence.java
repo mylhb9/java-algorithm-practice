cabdbedd63d4b00dccdfb629c28bee280f85deda
mylhb9 <mylhb9@naver.com>
2022-05-25 11:59:07 +0900

    알고리즘 보충 두수의 합

--- src/additiontype/SumKSubsequence.java
+++ src/additiontype/SumKSubsequence.java
@@ -10,36 +10,19 @@ public class SumKSubsequence {
         int K = sc.nextInt();
 
         int count = 0;
+
         Map<Integer, Integer> map = new HashMap<>();
         int[] arr = new int[N+1];
         int[] accArr = new int[N+1];
-        int sum = 0;
-        for(int i=1; i<=N; i++) {
-            arr[i] = sc.nextInt();
-            sum+=arr[i];
-        }
+
         map.put(0, 1);
         for(int i=1; i<=N; i++) {
+            arr[i] = sc.nextInt();
             accArr[i] += (arr[i] + accArr[i-1]);
+            count += map.getOrDefault(accArr[i]-K, 0);
             map.put(accArr[i], map.getOrDefault(accArr[i], 0)+1);
-            if(K>=0) {
-                if(accArr[i] >= K) {
-                    if(accArr[i] == K) {
-                        count+=map.getOrDefault(K, 0);
-                    } else {
-                        count+=map.getOrDefault(accArr[i]-K, 0);
-                    }
-                }
-            } else {
-                if(accArr[i] <= K) {
-                    if(accArr[i] == K) {
-                        count+=map.getOrDefault(K, 0);
-                    } else {
-                        count+=map.getOrDefault(accArr[i]-K, 0);
-                    }
-                }
-            }
         }
+
         System.out.println(count);
 
         sc.close();
--- src/additiontype/SumOfTwoNumbers.java
+++ src/additiontype/SumOfTwoNumbers.java
@@ -1,21 +1,22 @@
 package additiontype;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
         int K = sc.nextInt();
 
-
         int[] arr = new int[N];
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
         for(int i=0; i<N; i++) {
             arr[i] = sc.nextInt();
+            count += map.getOrDefault(K - arr[i], 0);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-
-
-
+        System.out.println(count);
     }
 }
